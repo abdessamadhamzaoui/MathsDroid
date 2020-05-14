5269709e11d11c32ec99f60bd0503c9c768a0898
Abdessamad Hamzaoui <abdessamadhamzaoui1@gmail.com>
2020-05-14 22:10:13 +0000

    errors solving

--- app/src/main/java/com/example/mathsdroid/MainActivity.java
+++ app/src/main/java/com/example/mathsdroid/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 import android.Manifest;
 import android.app.Notification;
 import android.app.NotificationChannel;
@@ -13,6 +14,8 @@ import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
+=======
+>>>>>>> parent of 01fd908... Create notification channel
 =======
 >>>>>>> parent of 01fd908... Create notification channel
 =======
@@ -38,10 +41,13 @@ public class MainActivity extends AppCompatActivity {
     Scanner scan = new Scanner(System.in);
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     Intent intent;
     private final String CHANNEL_ID = "Notifications";
     public Notification notification;
     public int nbrpremier= 2;
+=======
+>>>>>>> parent of 01fd908... Create notification channel
 =======
 >>>>>>> parent of 01fd908... Create notification channel
 =======
@@ -231,6 +237,9 @@ public class MainActivity extends AppCompatActivity {
             if (str_now.indexOf("greatest") != -1) {
                 str4 = (str2 + "\n" + "   greatest \n" + str);
 <<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> parent of 01fd908... Create notification channel
+=======
 >>>>>>> parent of 01fd908... Create notification channel
 =======
 >>>>>>> parent of 01fd908... Create notification channel
@@ -515,6 +524,7 @@ public class MainActivity extends AppCompatActivity {
             return 0;
     }
 <<<<<<< HEAD
+<<<<<<< HEAD
     public void updatenotif(int nbrpremier){
 
         notification = new NotificationCompat.Builder(this, CHANNEL_ID)
@@ -527,6 +537,8 @@ public class MainActivity extends AppCompatActivity {
         assert notificationManager != null;
         notificationManager.createNotificationChannel(notificationChannel);
     };
+=======
+>>>>>>> parent of 01fd908... Create notification channel
 =======
 >>>>>>> parent of 01fd908... Create notification channel
 
